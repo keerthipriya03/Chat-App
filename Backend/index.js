150cd2370abba678f43087e9609f0c8273fabd81
D.Keerthi Priya <dkeerthipriya6@gmail.com>
2026-06-16 15:04:57 +0530

    Updated for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,8 @@ const messageRoutes = require("./routes/messageRoutes");
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
+const path = require('path');
+
 dotenv.config({ quiet: true });
 connectDB();
 
@@ -22,7 +24,20 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+// -----deployment-----
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname1,'/frontend/build')));
 
+  app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+});
+} else{
+  app.get("/", (req,res) => {
+    res.send("API is running");
+  });
+}
+// -----deployment-----
 
 app.use(notFound);
 app.use(errorHandler);
